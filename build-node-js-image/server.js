023b793b325a98ee6df8cb18ea407b6550fa7758
Buyoung Chung <sunarol@naver.com>
2023-01-25 21:17:38 +0900

    fix: host port changed

--- build-node-js-image/server.js
+++ build-node-js-image/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const PORT = 8080;
-const HOST = "0.0.0.0";
 
 const app = express();
 app.get("/", (req, res) => {
@@ -9,5 +8,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+  console.log(`Running on http://localhost:${PORT}`);
 });
